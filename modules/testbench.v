daf3cd58034a374eb1e526b1af605fb84bb61552
shaazmik <daniil2003paramonov@mail.ru>
2024-05-04 17:12:42 +0000

    finished receiver

--- modules/testbench.v
+++ modules/testbench.v
@@ -8,7 +8,7 @@ parameter BAUDRATE = 115200;
 
 parameter ADDR_WIDTH = 5;
 
-parameter DATA_WIDTH = 7;
+parameter DATA_WIDTH = 8;
 
 reg clk = 1'b0;
 
@@ -37,12 +37,9 @@ rom #(.ADDR_WIDTH(ADDR_WIDTH), .WIDTH(DATA_WIDTH)) rom(
 );
 
 wire uart_next_addr;
-
 wire uart_tx_line;
-
 wire uart_tx_start;
 
-wire uart_rx_ready;
 
 uart_tx #(.CLK_FREQ(CLK_FREQ), .BAUDRATE(BAUDRATE), .DATA_WIDTH(DATA_WIDTH)) uart_tx(
     .clk(clk), 
@@ -52,26 +49,38 @@ uart_tx #(.CLK_FREQ(CLK_FREQ), .BAUDRATE(BAUDRATE), .DATA_WIDTH(DATA_WIDTH)) uar
     .next_addr(uart_next_addr)
 );
 
+wire [DATA_WIDTH-1:0]uart_rx_data;
+wire uart_rx_receiving;
+wire uart_rx_ready;
+
+
+uart_rx #(.CLK_FREQ(CLK_FREQ), .BAUDRATE(BAUDRATE), .DATA_WIDTH(DATA_WIDTH)) uart_rx(
+    .clk(clk), 
+    .tx_line(uart_tx_line),
+    .receive_data(uart_rx_data),
+    .is_receiving(uart_rx_receiving),
+    .is_finished(uart_rx_ready)
+);
 
 reg [2**ADDR_WIDTH - 1: 0] recv_ct = 0; // counter for recievied data
 reg [DATA_WIDTH - 1:0]recv_data = 0;  // data for printing
 
 always @(posedge clk) begin
     
-    recv_ct <= recv_ct + (uart_rx_ready);
-
-    recv_data <= receive_data;
+    if (uart_rx_ready) begin
+        recv_ct <= recv_ct + 1;
+        recv_data <= uart_rx_data;
+    end
 end
 
 initial begin
-    
     $monitor("[$monitor] recv_data[%02d]=0x%0h (%0c)", recv_ct, recv_data, recv_data);
 
     $dumpvars;
     
     $display("Test started...");
     
-    #10000 $finish;
+    #1024 $finish;
 end
 
 endmodule
--- modules/uart_rx.v
+++ modules/uart_rx.v
@@ -4,11 +4,75 @@ module uart_rx #(
     parameter DATA_WIDTH = 8        // Data width
 ) (
     input wire clk,
-    input wire reset,
-    input wire line,                 // RX line (serial input)
-    output reg [DATA_WIDTH-1:0] receive_data = 0, // Received data
-    output wire ready                // Ready signal indicating data reception is complete
+    input wire tx_line,          
+    
+    output reg [DATA_WIDTH-1:0] receive_data = 0, 
+    output reg is_receiving,         
+    output reg is_finished
 );
+
+
+localparam  RX_IDLE   = 3'd0,
+            RX_RECV   = 3'd1,
+            RX_LAST   = 3'd2,
+            RX_FINISH = 3'd3;
+
+initial begin
+    is_receiving = 0;
+    is_finished  = 0;
+    receive_data = 0;
+end
+
+wire rx_strobe;
+reg reset = 1'b0;
+// Clock division for baud rate adjustment
+clk_strobe #(
+    .DIV(CLK_FREQ / BAUDRATE)
+) clk_div (
+    .clk(clk), 
+    .strobe(rx_strobe),
+    .reset(reset)
+);
+
+
+reg [3:0]state   = RX_IDLE;
+reg [3:0]bit_num = 0;
+
+always @(posedge clk) begin
+    case (state)
+        RX_IDLE:
+            if (tx_line == 0) begin
+                state <= RX_RECV;
+                is_receiving <= 1;
+                is_finished <= 0;
+                reset <= 1'b1;        
+                 receive_data <= 0;
+                bit_num <= 0;
+            end
+        RX_RECV:
+            if (rx_strobe) begin
+                if (bit_num != DATA_WIDTH) begin
+                    receive_data[bit_num] <= tx_line;          
+                    bit_num <= bit_num + 1;
+                end
+                else begin
+                    state <= RX_LAST;
+                end
+            end
+        RX_LAST:
+            if (rx_strobe) begin
+                is_finished <= 1;
+                state <= RX_FINISH;
+            end
+        RX_FINISH: begin
+            state <= RX_IDLE;
+            is_receiving <= 0;
+            reset <= 0;
+        end
+        default: state <= RX_IDLE;
+    endcase
+end
+
 
 
 endmodule
